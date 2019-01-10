9266afa51edbb69e6f663686e2873575f6785ba0
Dan Ruch <danbruch@gmail.com>
2019-01-10 00:15:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function takeANumber(line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
+
+function nowServing(line) {
+  var msg = '';
+  if (line.length > 0) {
+    msg = line[0];
+    line = line.shift();
+  } else {
+    msg = 'There is nobody waiting to be served!';
+  }
+  return msg;
+}
